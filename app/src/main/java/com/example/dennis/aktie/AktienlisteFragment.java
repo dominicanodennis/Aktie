83f2b5947d5c1debf6d0a49ccfaa7a01e090fa6f
dominicanodennis <dominicanodennis@hotmail.de>
2015-09-10 19:53:02 +0200

    Initial commit

---
+++ app/src/main/java/com/example/dennis/aktie/AktienlisteFragment.java
@@ -0,0 +1,61 @@
+package com.example.dennis.aktie;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class AktienlisteFragment extends Fragment {
+
+    public AktienlisteFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        String LOG_TAG = AktienlisteFragment.class.getSimpleName();
+
+        Log.v(LOG_TAG, "verbose     - Meldung");
+        Log.d(LOG_TAG, "debug       - Meldung");
+        Log.i(LOG_TAG, "information - Meldung");
+        Log.w(LOG_TAG, "warning     - Meldung");
+        Log.e(LOG_TAG, "error       - Meldung");
+
+        String [] aktienlisteArray = {
+                "Adidas - Kurs: 73,45 €",
+                "Allianz - Kurs: 145,12 €",
+                "BASF - Kurs: 84,27 €",
+                "Bayer - Kurs: 128,60 €",
+                "Beiersdorf - Kurs: 80,55 €",
+                "BMW St. - Kurs: 104,11 €",
+                "Commerzbank - Kurs: 12,47 €",
+                "Continental - Kurs: 209,94 €",
+                "Daimler - Kurs: 84,33 €"
+        };
+        List<String> aktienListe = new ArrayList<>(Arrays.asList(aktienlisteArray));
+
+        ArrayAdapter<String> aktienlisteAdapter = new ArrayAdapter<>(getActivity(),
+                R.layout.list_item_aktienliste,R.id.list_item_aktienliste_textview,aktienListe);
+
+        View rootView =  inflater.inflate(R.layout.fragment_aktienliste, container, false);
+
+        ListView aktienlisteListView = (ListView) rootView.findViewById(R.id.listview_aktienliste);
+        aktienlisteListView.setAdapter(aktienlisteAdapter);
+
+        return  rootView;
+    }
+
+
+}
